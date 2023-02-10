3aaf031558471c6f5bf14da8b5dcb7da9fd56a14
kangjisung <kangjisung@naver.com>
2023-02-10 15:42:29 +0900

    23/2/10 publicRoute privateRoute

---
+++ ch06/ch06_3/src/routes/RoutesSetup.tsx
@@ -0,0 +1,39 @@
+import {Routes, Route} from 'react-router-dom'
+import Layout from './Layout'
+import LandingPage from './LandingPage'
+import Board from '../pages/Board'
+import NoMatch from './NoMatch'
+import Signup from './Auth/SignUp'
+import Login from './Auth/Login'
+import Logout from './Auth/Login'
+import RequireAuth from './Auth/ResqireAuth'
+
+export default function RoutesSetup() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<LandingPage />} />
+        <Route
+          path="/board"
+          element={
+            <RequireAuth>
+              <Board />
+            </RequireAuth>
+          }
+        />
+        <Route path="*" element={<NoMatch />} />
+      </Route>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/logout"
+        element={
+          <RequireAuth>
+            <Logout />
+          </RequireAuth>
+        }
+      />
+      <Route path="*" element={<NoMatch />} />
+    </Routes>
+  )
+}
