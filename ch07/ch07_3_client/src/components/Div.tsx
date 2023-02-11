77e1995d8d87e3df13cac8eba4b5e28839525c05
kangjisung <kangjisung@naver.com>
2023-02-11 11:34:15 +0900

    23/2/11 express api server

---
+++ ch07/ch07_3_client/src/components/Div.tsx
@@ -0,0 +1,44 @@
+import type {FC, DetailedHTMLProps, HTMLAttributes, PropsWithChildren} from 'react'
+import type {WidthHeight} from './WidthHeight'
+import type {LeftRightTopBottom} from './LeftRightTopBottom'
+import type {MinMaxWidthHeight} from './MinMaxWidthHeight'
+
+export type ReactDivProps = DetailedHTMLProps<
+  HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>
+export type DivProps = ReactDivProps &
+  PropsWithChildren<WidthHeight> &
+  LeftRightTopBottom &
+  MinMaxWidthHeight & {
+    src?: string
+  }
+
+// prettier-ignore
+export const Div: FC<DivProps> = ({
+  width,
+  height,
+  style: _style,
+  src,
+  className: _className,
+  left,
+  right,
+  top,
+  bottom,
+  minWidth, maxWidth, maxheight, minHeight,
+  ...props
+}) => {
+  const style = {
+    ..._style,
+    width,
+    height,
+    backgroundImage: src && `url(${src})`,
+    left,
+    right,
+    top,
+    bottom,
+    minWidth, maxWidth, maxheight, minHeight
+  }
+  const className = ['box-sizing', src && 'bg-gray-300', _className].join(' ')
+  return <div {...props} className={className} style={style} />
+}
